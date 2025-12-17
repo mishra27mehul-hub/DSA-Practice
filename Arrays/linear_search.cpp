ca985991c7e99aff7c10c96aafe3b97928d67c24
mishra27mehul-hub <mishra27mehul@gmail.com>
2025-12-18 01:10:02 +0530

    Added linear search solution

---
+++ Arrays/linear_search.cpp
@@ -0,0 +1,30 @@
+// Problem: Linear Search 
+// Approach: Traverse array and compare each element with target
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int arr[]={1,2,3,4};
+    int target;
+    cout<<"the target is :";
+    cin>>target;
+
+    bool found=false;
+
+    for(int i=0;i<4;i++){
+         if(arr[i]==target){
+            found=true;
+         }
+    }
+    if(found){
+cout<<"FOUND";
+    }
+    else{
+        cout<<"NOT FOUND";
+    }
+    return 0;
+}
