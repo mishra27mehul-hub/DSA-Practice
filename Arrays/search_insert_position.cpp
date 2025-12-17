7ee593aaebf9761552d64678d8783340f2d0bf0e
mishra27mehul-hub <mishra27mehul@gmail.com>
2025-12-18 01:55:31 +0530

    Added search insert position solution

---
+++ Arrays/search_insert_position.cpp
@@ -0,0 +1,27 @@
+// Problem: Search Insert Position
+// Approach: Linear scan on already sorted array
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 3, 5, 6};
+    int target;
+    cin >> target;
+
+    int index = 4;   // it is 4 because of size of array 
+  // one more than index of array to insert in case target is greater than 3
+  
+
+    for(int i = 0; i < 4; i++) {
+        if(arr[i] >= target) {
+            index = i;
+            break;
+        }
+    }
+// break statement tells us to stop and print if we get desired target 
+    cout << index;
+    return 0;
+}
