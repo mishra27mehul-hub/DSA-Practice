3d591ffb80e598591e10bbc406206a8dac3df0e2
mishra27mehul-hub <mishra27mehul@gmail.com>
2025-12-19 19:46:42 +0530

    Added max_element.cpp

---
+++ Arrays/max_element.cpp
@@ -0,0 +1,32 @@
+// Problem: Find maximum element in an array
+// Approach: Traverse array and keep updating maximum
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"number of elements in array:";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int maxElement=arr[0];//first assume 1st element is max
+
+    for(int i=0;i<n;i++){//check others if they are larger
+        if(maxElement<arr[i]){
+            maxElement=arr[i];//update
+
+        
+        }
+
+    }
+    cout<<"the max element is:"<<maxElement<<endl;
+   
+    return 0;
+}
